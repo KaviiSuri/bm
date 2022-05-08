71efccedee1a0cf627140e2e38a38a065549c728
KaviiSuri <surikavii@gmail.com>
2022-05-08 19:38:01 +0530

    implemented -l <limit> for bme

--- src/bin/bme.rs
+++ src/bin/bme.rs
@@ -1,9 +1,7 @@
 use bm::BM;
 use std::{fs::File, process};
 
-const BM_EXECUTION_LIMIT: usize = 69;
-
-static USAGE: &'static str = "Usage: ./bme <input_file>.bm";
+static USAGE: &'static str = "Usage: ./bme <input_file>.bm [-l <limit>]";
 
 fn main() {
     let mut args = std::env::args();
@@ -16,12 +14,22 @@ fn main() {
         )
         .expect("Could not read input file.");
 
-    let program = BM::deserialize_program_from(input_file);
+    // parse limtt
+    let limit = match args.next() {
+        Some(l) if l == "-l" => Some(
+            args.next()
+                .expect(format!("Expected a limit after -l\n {}", USAGE).as_str())
+                .parse::<usize>()
+                .expect(format!("limit must be an usigned integer\n {}", USAGE).as_str()),
+        ),
+        Some(l) => panic!("Unknown flag {}\n{}", l, USAGE),
+        None => None,
+    };
 
+    let program = BM::deserialize_program_from(input_file);
     let mut bm: BM = Default::default();
     bm.load_program_from_memory(program.as_slice());
-
-    match bm.execute_program(Some(BM_EXECUTION_LIMIT)) {
+    match bm.execute_program(limit) {
         Ok(()) => bm.dump_stack(&mut std::io::stdout()).expect("should work"),
         Err(e) => {
             eprintln!("{}", e);
